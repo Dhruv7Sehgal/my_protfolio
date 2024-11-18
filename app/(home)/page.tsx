0c09071ae7718766731516b96cf59947fd6d6edf
Dhruv7Sehgal <dhruvs123ehgal@gmail.com>
2024-11-18 12:31:12 +0530

    added sheet in nav

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { BackgroundLinesDemo } from "@/components/BackgroundLinesDemo";
 import AboutUs from "./aboutus/page";
 import { WobbleCardDemo } from "@/components/WobbleCardDemo";
-import { BentoGridDemo } from "@/components/BentoGridDemo";
+// import { BentoGridDemo } from "@/components/BentoGridDemo";
 
 const Page = () => {
   return (
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com"], // Add Unsplash domain here
+    domains: ["assets.aceternity.com"],
   },
 };
 

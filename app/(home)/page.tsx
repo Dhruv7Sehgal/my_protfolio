2f76958cbc5c38350f3f9660a17225fff6fabe49
Dhruv7Sehgal <dhruvs123ehgal@gmail.com>
2024-11-18 12:45:59 +0530

    cahnges

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import { BackgroundLinesDemo } from "@/components/BackgroundLinesDemo";
 import AboutUs from "./aboutus/page";
-import { WobbleCardDemo } from "@/components/WobbleCardDemo";
-// import { BentoGridDemo } from "@/components/BentoGridDemo";
+// import { WobbleCardDemo } from "@/components/WobbleCardDemo";
+import { BentoGridDemo } from "@/components/BentoGridDemo";
 
 const Page = () => {
   return (
@@ -11,13 +11,8 @@ const Page = () => {
       <section className="w-full dark:bg-black">
         <BackgroundLinesDemo />
         <AboutUs />
-        <div className="mt-36 mb-6 ">
-          {/* <BentoGridDemo /> */}
-          <WobbleCardDemo />
-        </div>
         <div className="mb-44">
-          {" "}
-          <WobbleCardDemo />
+          <BentoGridDemo />
         </div>
       </section>
     </>
